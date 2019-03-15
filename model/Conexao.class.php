af47c24a2fafcb970b8083c790a6d167c5210c14
LucasRafaelBalduino <42643459+LucasRafaelBalduino@users.noreply.github.com>
2019-03-15 14:39:14 -0300

    Initial commit

---
+++ model/Conexao.class.php
@@ -0,0 +1,22 @@
+<?php  
+/**
+ * classe de conexao,responsável por inciar a conexao com o banco de dados
+ */
+
+class Conexao {
+
+	public static $instance;
+
+	public static function get_instance() {
+		if(!isset(self::$instance)) {
+			self::$instance = new PDO("mysql:host=localhost;dbname=agendamentos;", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+
+		return self::$instance;
+
+	}
+
+}
+
+?>
